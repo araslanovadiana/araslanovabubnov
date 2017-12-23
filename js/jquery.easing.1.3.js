b3a3646f8dbaed646ccc242b2a6a4f7bae4d4793
araslanovabubnov <34652534+araslanovabubnov@users.noreply.github.com>
2017-12-23 17:19:09 +0300

    Update jquery.easing.1.3.js

--- js/jquery.easing.1.3.js
+++ js/jquery.easing.1.3.js
@@ -175,7 +175,6 @@ var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
 
-  // Change to your service ID, or keep using the default service
   var service_id = "yandex";
   var template_id = "template_wbGwzo7b";
 
